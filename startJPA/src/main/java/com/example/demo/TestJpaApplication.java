061bf2fecdecc492cd9e9404a5e0143da1e19439
강민욱 <@DESKTOP-9E3U1AS>
2022-06-08 16:35:27 +0900

    메인클래스 제거

--- startJPA/src/main/java/com/example/demo/TestJpaApplication.java
+++ startJPA/src/main/java/com/example/demo/TestJpaApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestJpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestJpaApplication.class, args);
-	}
+	/*
+	 * public static void main(String[] args) {
+	 * SpringApplication.run(TestJpaApplication.class, args); }
+	 */
 
 }
